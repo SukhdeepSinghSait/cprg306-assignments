44b82f116c534d03a124077f909b5ed03b27ec51
Sukhdeep Singh <Sukhdeep.Singh02@edu.sait.ca>
2024-09-12 16:28:32 -0600

    Finished 2nd Assignment
    Added week 2 folder and page.js as well as StudentInfo.js in week 2 folder. Cleaned up root page.js and added some code to it.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Sukhdeep Singh</h1>
+            <Link href= "https://github.com/SukhdeepSinghSait">https://github.com</Link>
+        </main>
+    );
+}
